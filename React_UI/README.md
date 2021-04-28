788093b3f213526afb07bc661dc13901840c3124
KrishanSiva <kirioffl43@gmail.com>
2021-04-28 20:15:53 +0530

    initial commit

---
+++ React_UI/README.md
@@ -0,0 +1,8 @@
+https://maps.googleapis.com/maps/api/js?v=3.exp&libraries=geometry,drawing,places
+# Google Maps in React
+
+YouTube video can be found here: [https://youtu.be/Pf7g32CwX_s](https://youtu.be/Pf7g32CwX_s)
+
+## Running Locally
+
+Google Maps API Key: Create a new API key from the Google developer console, making sure that `Maps JavaScript API` has been enabled. Copy that key into your `.env.local` file creating an ENV var with the name `REACT_APP_GOOGLE_KEY`.
---
+++ React_UI/build/precache-manifest.d18b7fb011cfb50b7f1c8a09a01341e2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "52e64bf60bc790563961",
+    "url": "/static/css/main.b5872f35.chunk.css"
+  },
+  {
+    "revision": "52e64bf60bc790563961",
+    "url": "/static/js/main.7388a1da.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "42ce9c43cb69e9a6144d",
+    "url": "/static/js/2.6555a174.chunk.js"
+  },
+  {
+    "revision": "c07dfbe111d755b0006b4fbce76b114f",
+    "url": "/index.html"
+  }
+];
